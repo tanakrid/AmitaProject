0e1ba0aff0908feb37c9ab00ef6a854f66718314
chiney <tanakrid.c@ku.th>
2019-09-29 17:12:10 +0700

    first commit

---
+++ resources/views/layouts/Home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title> Amita project </title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="{{ url('css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="{{ url('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  
+  @include('layouts._nonLoginNavbar')
+
+  @yield('content')
+
+  @include('layouts._footer')
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="{{ url('js/materialize.js') }}"></script>
+  <script src="{{ url('js/init.js') }}"></script>
+
+  </body>
+</html>
---
+++ resources/views/layouts/_nonLoginNavbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="light-green accent-3" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">Logo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#"> Login </a></li>
+        <li><a href="#"> Register </a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger light-green"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
